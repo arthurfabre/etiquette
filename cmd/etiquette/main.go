286805e2a041baf2e22dc5eccbc42f1e657a27da
Arthur Fabre <arthur@arthurfabre.com>
2024-02-05 20:22:42 +0100

    etiquette: Add -status flag, help output
    Switch from using os.Args[1] to flag to provide more user friendly
    output.
    
    Add a status flag to see what a printer is up to / what media is loaded
    without printing anything out.

--- cmd/etiquette/main.go
+++ cmd/etiquette/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"image"
 	"io"
@@ -16,13 +17,30 @@ import (
 )
 
 func main() {
-	if err := print(os.Args[1], os.Stdin); err != nil {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `%s [options] /dev/usb/lpN
+
+Print each line from stdin as a text label on a Brother PT-700 printer connected as /dev/usb/lpN.
+
+`, os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	var status = flag.Bool("status", false, "Show printer status only, don't print anything.")
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(-1)
+	}
+
+	if err := print(flag.Arg(0), *status, os.Stdin); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(-1)
 	}
 }
 
-func print(printerPath string, labels io.Reader) error {
+func print(printerPath string, printStatus bool, labels io.Reader) error {
 	printer, err := pt700.Open(printerPath)
 	if err != nil {
 		return err
@@ -33,6 +51,11 @@ func print(printerPath string, labels io.Reader) error {
 	if err != nil {
 		return err
 	}
+	if printStatus {
+		fmt.Printf("%+v\n", status)
+		return nil
+	}
+
 	if err := status.Err(); err != nil {
 		return err
 	}
